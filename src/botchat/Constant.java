a354a43a59578a1c13cb126bdfadd2a01b60e20b
Hoang Van Tuan <hoang_van_tuan@mediado.jp>
2016-12-22 16:47:09 +0900

    add talk

--- src/botchat/Constant.java
+++ src/botchat/Constant.java
@@ -14,6 +14,14 @@ public class Constant {
 	public final static boolean SUCCESS = true;
 	public final static boolean FAILL = false;
 	public final static String HELP = "Syntax of BOT : bot command; @bot command; bot:command";
+	public final static String TALK = "talk";
 
+	// Message
+	public final static String SAYANYTHING = "What did you say ?";
+	public final static String[] talk = { "What?", "Who are you?", "Oh yeah, Today is beautifulday", "You are GREAT!",
+			"You are Handsome :))", "Happy to know you <3", "Im was made by Tuan !", "Yes I think So",
+			"....I dont know", "I know who you are", "Are you understand what did i say?", "I love you so much",
+			"What's up?", "What have you been doing?", " I was just thinking", "There's no way to know.",
+			"I got it.", };
 
 }
--- src/botchat/Helpers.java
+++ src/botchat/Helpers.java
@@ -1,5 +1,7 @@
 package botchat;
 
+import java.util.Random;
+
 public class Helpers {
 
 	public static Response getResponse(String message) {
@@ -16,13 +18,13 @@ public class Helpers {
 			res.setCommand(Constant.HELP);
 		} else if (message.startsWith(Constant.BOT1) && message.charAt(3) == ' ') {
 			res.setType(Constant.BOT);
-			res.setCommand(message.substring(4));
+			res.setCommand(message.substring(4).trim());
 		} else if (message.startsWith(Constant.BOT2) && message.charAt(4) == ' ') {
 			res.setType(Constant.BOT);
-			res.setCommand(message.substring(5));
+			res.setCommand(message.substring(5).trim());
 		} else if (message.startsWith(Constant.BOT3)) {
 			res.setType(Constant.BOT);
-			res.setCommand(message.substring(4));
+			res.setCommand(message.substring(4).trim());
 		} else
 			res.setType(Constant.MESSAGE);
 		return res;
@@ -33,6 +35,8 @@ public class Helpers {
 		if (res.getType().equals(Constant.BOT)) {
 			if (Constant.PING.equals(res.getCommand().trim())) {
 				res.setText(Constant.PONG);
+			} else if (Helpers.isTalk(res)) {
+				res.setText(Helpers.getTalk(res));
 			} else {
 				res.setText(res.getCommand());
 			}
@@ -45,5 +49,26 @@ public class Helpers {
 			}
 		}
 		return res;
+	}
+
+	public static boolean isTalk(Response res) {
+		if (Constant.TALK.equals(res.getCommand()))
+			return true;
+		else if (res.getCommand().startsWith(Constant.TALK) && res.getCommand().charAt(4) == ' ') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static String getTalk(Response res) {
+
+		if (Constant.TALK.equals(res.getCommand())) {
+			return Constant.SAYANYTHING;
+		} else {
+			Random rand = new Random();
+			int n = rand.nextInt(Constant.talk.length - 1);
+			return Constant.talk[n];
+		}
 	}
 }
