f68d86989738a338ac9bee497568bab9c0c076db
Hoang Van Tuan <hoang_van_tuan@mediado.jp>
2016-12-22 21:02:37 +0900

    test

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var ws = new WebSocket("wss://" + location.host + "/bot");
+var ws = new WebSocket("wss://" + location.host + "/botchat/bot");
 
 $(function () {
   $('form').submit(function(){
